b88a8c03dcac443d21ef10b9ab02cf06c118f74c
Pedro Mendes <pedromendescraft@gmail.com>
2022-02-28 12:32:43 -0300

    feat(parsing): Lexer to store token location
    `remove_comments` function now is a lexer, remove comments and store the token location of the original file

--- src/parsing.rs
+++ src/parsing.rs
@@ -12,31 +12,35 @@ pub enum Instruction {
     LoopEnd(usize),
 }
 
-fn remove_comments(contents: &str) -> String {
-    let mut result = String::new();
-    for content in contents.chars() {
-        match content {
-            '>' | '<' | '+' | '-' | '.' | ',' | '[' | ']' => {
-                result.push(content);
+type Token = (char, (usize, usize));
+
+fn lexer(contents: &str) -> Vec<Token> {
+    let mut result = Vec::new();
+    for (line_index, line) in contents.lines().enumerate() {
+        for (char_index, content) in line.chars().enumerate() {
+            match content {
+                '>' | '<' | '+' | '-' | '.' | ',' | '[' | ']' => {
+                    result.push((content, (line_index + 1, char_index + 1)));
+                }
+                _ => {}
             }
-            _ => {}
         }
     }
     result
 }
 
-fn create_loop_targets(instructions_str: &str) -> AHashMap<usize, usize> {
+fn create_loop_targets(tokens: &[Token]) -> AHashMap<usize, usize> {
     let mut loop_stack = Vec::new();
     let mut targets = AHashMap::new();
-    for (index, instruction) in instructions_str.chars().enumerate() {
+    for (index, instruction) in tokens.iter().enumerate() {
         match instruction {
-            '[' => {
+            ('[', _) => {
                 loop_stack.push(index);
             }
-            ']' => {
-                let loop_index = loop_stack
-                    .pop()
-                    .unwrap_or_else(|| panic!("Unmatched loop end at index {}", index));
+            (']', location) => {
+                let loop_index = loop_stack.pop().unwrap_or_else(|| {
+                    panic!("Unmatched loop end at index {}:{}", location.0, location.1)
+                });
                 targets.insert(loop_index, index);
                 targets.insert(index, loop_index);
             }
@@ -48,22 +52,22 @@ fn create_loop_targets(instructions_str: &str) -> AHashMap<usize, usize> {
 
 pub fn parse_instructions(instructions_str: &str) -> Vec<Instruction> {
     let mut instructions = Vec::new();
-    let clean_instructions_str = remove_comments(instructions_str);
-    let loop_targets = create_loop_targets(&clean_instructions_str);
-    for (index, instruction_char) in clean_instructions_str.chars().enumerate() {
+    let tokens = lexer(instructions_str);
+    let loop_targets = create_loop_targets(&tokens);
+    for (index, instruction_char) in tokens.iter().enumerate() {
         match instruction_char {
-            '>' => instructions.push(Instruction::IncrementPointer),
-            '<' => instructions.push(Instruction::DecrementPointer),
-            '+' => instructions.push(Instruction::IncrementValue),
-            '-' => instructions.push(Instruction::DecrementValue),
-            '.' => instructions.push(Instruction::Output),
-            ',' => instructions.push(Instruction::Input),
-            '[' => instructions.push(Instruction::LoopStart(
-                *loop_targets
-                    .get(&index)
-                    .unwrap_or_else(|| panic!("Unmatched '[' at index: {}", index)),
+            ('>', _) => instructions.push(Instruction::IncrementPointer),
+            ('<', _) => instructions.push(Instruction::DecrementPointer),
+            ('+', _) => instructions.push(Instruction::IncrementValue),
+            ('-', _) => instructions.push(Instruction::DecrementValue),
+            ('.', _) => instructions.push(Instruction::Output),
+            (',', _) => instructions.push(Instruction::Input),
+            ('[', location) => instructions.push(Instruction::LoopStart(
+                *loop_targets.get(&index).unwrap_or_else(|| {
+                    panic!("Unmatched '[' at index: {}:{}", location.0, location.1)
+                }),
             )),
-            ']' => instructions.push(Instruction::LoopEnd(*loop_targets.get(&index).unwrap())),
+            (']', _) => instructions.push(Instruction::LoopEnd(*loop_targets.get(&index).unwrap())),
             _ => {}
         }
     }
