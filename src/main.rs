48fd8ada64d521267fdaf9629ff266e2d75e22ed
Pedro Mendes <pedromendescraft@gmail.com>
2022-02-28 00:39:12 -0300

    feat: Using clap argument parser
    Making subcommand `interpret` to organize program features

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
-mod parsing;
 mod interpreter;
+mod parsing;
 
-use parsing::parse_instructions;
 use interpreter::execute;
+use parsing::parse_instructions;
+
+use anyhow::Result;
+use clap::Parser;
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::path::PathBuf;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file = File::open("./brainfuck/helloworld.bf")?;
-    let mut contents = String::new();
-    let mut buffer = BufReader::new(file);
-    buffer.read_to_string(&mut contents)?;
-    let instructions = parse_instructions(&contents);
-    execute(&instructions);
+#[derive(Parser)]
+#[clap(about, author, version)]
+enum Opt {
+    /// Interprets the brainfuck program in the given file
+    Interpret {
+        /// The brainfuck file to interpret
+        file: PathBuf,
+    },
+}
+
+fn main() -> Result<()> {
+    let opt = Opt::parse();
+    match opt {
+        Opt::Interpret { file } => {
+            let file = File::open(file)?;
+            let mut contents = String::new();
+            let mut buffer = BufReader::new(file);
+            buffer.read_to_string(&mut contents)?;
+            let instructions = parse_instructions(&contents);
+            execute(&instructions);
+        }
+    }
     Ok(())
 }
